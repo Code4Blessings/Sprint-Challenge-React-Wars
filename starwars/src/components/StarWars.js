2f6e1017e1e3e4420f67b78f94ca5b6f62591495
Code4Blessings <robinwarden@code4blessings.com>
2019-11-08 11:56:05 -0500

    Setup Star Wars files

---
+++ starwars/src/components/StarWars.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+
+
+const StarWars = () => {
+    return (
+        <div>
+        
+        </div>
+    )
+}
+
+export default StarWars;
---
+++ starwars/src/components/StarWarsCard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const StarWarsCard = () => {
+    return (
+        <div>
+        
+        </div>
+    )
+}
+
+export default StarWarsCard;
