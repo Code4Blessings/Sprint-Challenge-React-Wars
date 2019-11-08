176fbb7759e1cba66ca888274113e41b6d774d25
Code4Blessings <robinwarden@code4blessings.com>
2019-11-08 13:56:53 -0500

    Completed build of StarWars components

--- starwars/src/components/StarWars.js
+++ starwars/src/components/StarWars.js
@@ -1,12 +1,45 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
+import StarWarsCard from "./StarWarsCard";
+
+
 
 
 const StarWars = () => {
+
+    const [people, setPeople] = useState([]);
+    const API = "https://corsanywhere.herokuapp.com/https://lambda-swapi.herokuapp.com/api/people/"
+
+    useEffect(() => {
+        axios
+        .get(API)
+        .then((response) => {
+            setPeople(response.data.results);
+            console.log(response.data.results);
+        })
+        .catch((error) => {
+            console.log("Data has returned an error", error);
+        })
+    },[])
+    
     return (
-        <div>
-        
-        </div>
-    )
+      <div className="starWars-cast">
+        {people.map((person, index) => {
+        return (
+          <StarWarsCard
+            key={index}
+            name={person.name}
+            gender={person.gender}
+            haircolor={person.haircolor}
+            height={person.height}
+          />
+        );
+      })}
+      </div>
+      
+    
+
+    );
 }
 
 export default StarWars;
--- starwars/src/components/StarWarsCard.js
+++ starwars/src/components/StarWarsCard.js
@@ -1,9 +1,15 @@
 import React from "react";
+import "./StarWars.css";
+import Styled from "styled-components";
 
-const StarWarsCard = () => {
+const StarWarsCard = (props) => {
+    const {name, gender, haircolor, height} = props;
     return (
-        <div>
-        
+        <div className="star-card" >
+            <h2>{name}</h2>
+            <p>{gender}</p>
+            <p>{haircolor}</p>
+            <p>{height}</p>
         </div>
     )
 }
